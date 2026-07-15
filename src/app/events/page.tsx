1dfb5b2177e099bc8e11e7d9094d463a8282b862
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-07-15 12:54:44 +0600

    Fexed deployment error by adding suspense

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+import EventCardSkeleton from "./components/EventCardSkeleton";
 import EventsExplorer from "./components/EventsExplorer";
 import PageHeader from "./components/PageHeader";
 
@@ -5,7 +7,9 @@ const ExploreEventsPage = () => {
   return (
     <main className="mx-auto flex w-full max-w-7xl flex-col gap-8 bg-background px-4 py-10 text-foreground sm:px-6 lg:px-8">
       <PageHeader />
-      <EventsExplorer />
+      <Suspense fallback={<EventCardSkeleton />}>
+        <EventsExplorer />
+      </Suspense>
     </main>
   );
 };
